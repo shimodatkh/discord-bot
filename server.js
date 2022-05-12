eec4524136e2aa19d12efa71bd25f5c802072e0c
fratty <tech150220@gmail.com>
2022-05-12 23:08:13 +0900

    a

--- server.js
+++ server.js
@@ -86,11 +86,11 @@ client.on('message', message =>{
   }
 
   if (message.isMemberMentioned(client.user) && message.content.match(/予想/) && message.content.match(/頭/)){
-    console.log(message.content.match(/予想(?<digit>[0-9])/));
-    console.log(message.content.match(/予想(?<digit>[0-9])/).groups.digit);
+    console.log(message.content.match(/予想(?<digit>[^0-9])/));
+    console.log(message.content.match(/予想(?<digit>[^0-9])/).groups.digit);
     const num_uma =  Number(message.content.replace(/[^0-9]/g, ''));
     console.log("予想 頭数："+num_uma);
-    lotteryRace(message.channel.id,num_uma);
+    lotteryRace(message.channel.id,message.content.match(/予想(?<digit>[^0-9])/).groups.digit);
   }
 
   if ((message.content.match(/^！シャイニー/)) ||
